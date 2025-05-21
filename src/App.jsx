706d93dc0629f07194ae949ee782921a519fd7fe
Wilton24 <cbasher24@gmail.com>
2025-05-21 22:04:38 +0800

    added NoProjectSelected component and custom Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import Sidebar from "./components/Sidebar";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import { useState } from "react";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleAddStartProject(){
+    setProjectsState(prevState =>{
+      return {
+        selectedProjectId: null,
+        ...prevState,        
+      }
+    })
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <NewProject  />
+      <Sidebar onStartAddProject={handleAddStartProject}/>
+      <NoProjectSelected onStartAddProject={handleAddStartProject} />
+      
     </main>
   );
 }
