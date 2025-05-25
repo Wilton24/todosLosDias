44a853f70c6f92cc6fe607112ebc8ce1a5bbcd06
Wilton24 <cbasher24@gmail.com>
2025-05-25 22:55:38 +0800

    created New Task component for the mini tasks

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,10 +1,26 @@
+import { useRef, useState } from "react"
+
 export default function NewTask(){
+  const [enteredTask, setEnteredTask] = useState();
+
+  function logTask(e){
+    setEnteredTask(e.target.value)         
+  };
+
+  function handleTask(){
+    setEnteredTask('');
+  }
+
   return (
     <div className="flex items-center gap-4">
       <input 
+        onChange={()=>logTask(event)}
         className="w-64 px-2 py-1 rounded-sm bg-stone-200"
-        type="text" />
-      <button className="text-stone-700 hover:text-stone-950">Add Task</button>
+        type="text"
+        value={enteredTask} />
+      <button 
+        onClick={()=> handleTask()}
+        className="text-stone-700 px-2 py-1 rounded-md hover:bg-stone-400 hover:text-stone-950">Add Task</button>
     </div>
   )
 }
